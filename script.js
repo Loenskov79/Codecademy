2f79fb505c31a6b3dc28a9236900b0313d93e2d4
Loenskov <victor.moeller@outlook.dk>
2024-04-08 18:45:25 +0200

    First commit

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-let randomMessage = ['']
+const randomQuoteGenerator = () => {
+    let messageArray = ["It takes courage to grow up and become who you really are.", "Your self-worth is determined by you. You dont have to depend on someone telling you who you are.",
+    "Nothing is impossible. The word itself says I'm possible!", "Champions keep playing until they get it right.", "Believe you can and you're halfway there."];
+    let i = Math.floor(Math.random() * messageArray.length);
+    return messageArray[i];
+}
 
-console.log('Test');
+
+const printMotivationalQuote = () => {
+    console.log(randomQuoteGenerator());
+}
+
+printMotivationalQuote();
